dfdca35baa9951c150e587c64f73aff4a624b2e2
Mike <mail@website-company.com>
2017-04-30 21:49:07 -0400

    Add files via upload
    Initial files for the boilerplate. Run the install commands to install dependency.

---
+++ Gruntfile.js
@@ -0,0 +1,100 @@
+/**
+ * File For: my-boilerplate
+ * File Name: .
+ * Author: Mike Giammattei
+ * Created On: 4/30/2017, 8:20 PM
+ */
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Copy web assets from bower_components to more convenient directories.
+        copy: {
+            main: {
+                files: [
+                    // Vendor scripts.
+                    {
+                        expand: true,
+                        cwd: 'bower_components/bootstrap-sass/assets/javascripts/',
+                        src: ['**/*.js'],
+                        dest: 'scripts/bootstrap-sass/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'bower_components/jquery/dist/',
+                        src: ['**/*.js', '**/*.map'],
+                        dest: 'scripts/jquery/'
+                    },
+
+                    // Fonts.
+                    {
+                        expand: true,
+                        filter: 'isFile',
+                        flatten: true,
+                        cwd: 'bower_components/',
+                        src: ['bootstrap-sass/assets/fonts/**'],
+                        dest: 'fonts/'
+                    },
+
+                    // Stylesheets
+                    {
+                        expand: true,
+                        cwd: 'bower_components/bootstrap-sass/assets/stylesheets/',
+                        src: ['**/*.scss'],
+                        dest: 'scss/'
+                    },
+                    // FontAwesome
+                    {
+                        expand: true,
+                        cwd: 'bower_components/font-awesome/scss',
+                        src: ['**/*.scss'],
+                        dest: 'scss/font-awesome'
+                    },
+                    // FontAwesome fonts
+                    {
+                        expand: true,
+                        cwd: 'bower_components/font-awesome/fonts',
+                        src: ['**'],
+                        dest: 'fonts/font-awesome'
+                    }
+                ]
+            },
+        },
+
+        // Compile SASS files into minified CSS.
+        sass: {
+            options: {
+                includePaths: ['bower_components/bootstrap-sass/assets/stylesheets']
+            },
+            dist: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    'css/app.css': 'scss/app.scss'
+                }
+            }
+        },
+
+        // Watch these files and notify of changes.
+        watch: {
+            grunt: { files: ['Gruntfile.js'] },
+
+            sass: {
+                files: [
+                    'scss/**/*.scss'
+                ],
+                tasks: ['sass']
+            }
+        }
+    });
+
+    // Load externally defined tasks.
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Establish tasks we can run from the terminal.
+    grunt.registerTask('build', ['sass', 'copy']);
+    grunt.registerTask('default', ['build', 'watch']);
+}
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * File For: my-boilerplate
+ * File Name: index.php.
+ * Author: Mike Giammattei
+ * Created On: 4/30/2017, 8:39 PM
+ */;
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="scss/site.css">
+    <script type="text/javascript" src="scripts/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="scripts/bootstrap-sass/bootstrap.js"></script>
+    <style>
+        /* Note: Try to remove the following lines to see the effect of CSS positioning */
+        .affix {
+            top: 0;
+            width: 100%;
+        }
+
+        .affix + .container-fluid {
+            padding-top: 70px;
+        }
+    </style>
+</head>
+<body>
+<i class="fa fa-thermometer-empty" aria-hidden="true"></i>
+</body>
+</html>
